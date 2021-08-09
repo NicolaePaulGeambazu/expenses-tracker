8420b954356d97822aae55d5fb4a147ad9e5033c
paulgsm <58441846+paulgsm@users.noreply.github.com>
2021-08-09 12:32:38 +0100

    FirstStage

--- src/App.js
+++ src/App.js
@@ -39,10 +39,10 @@ function App() {
   <Card>
     
 
-    <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].data}> </ExpenseItem>;
-    <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].data}> </ExpenseItem>;
-    <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].data}> </ExpenseItem>;
-    <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].data}> </ExpenseItem>;
+    <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].data}> </ExpenseItem>
+    <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].data}> </ExpenseItem>
+    <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].data}> </ExpenseItem>
+    <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].data}> </ExpenseItem>
     <ExpenseItem title={expenses[4].title} amount={expenses[4].amount} date={expenses[4].data}> </ExpenseItem>
   </Card>
   )
